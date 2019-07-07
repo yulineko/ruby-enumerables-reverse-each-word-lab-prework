63cf462c7efa9dfbe96d6b262a7a87ca39d438f0
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-07-07 22:24:36 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+def reverse_each_word(sentence)
+  array = sentence.split(" ") 
+  reverse_array = []
+  
+  array.collect do |sentence| 
+    reverse_array << sentence.reverse 
+  end
+  
+  reverse_array.join(" ")
+end
