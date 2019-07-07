7965fa840c45466146e6f5922824d88e982f407f
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-07-07 22:19:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,3 @@
 def reverse_each_word(sentence)
-  sentence = sentence.split('')
-  reverse_sentence = []
-  
-  sentence.each do character 
-    reverse_sentence.unshift(character)
-  end
-  
-  return reverse_sentence.join(''
+ 
 end
